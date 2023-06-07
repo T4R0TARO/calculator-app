c55b032b4d401d7de89d665e037ec02f15d0cb21
josh manansala <joshmanansalagsd@gmail.com>
2023-06-06 19:23:50 -0700

    styles update to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import "./App.css";
 import TextAnimation from "./components/TextAnimation";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleButtonClick(value) {
+    console.log(value);
+  }
+
   return (
     <>
-      <TextAnimation />
+      <div className="calculator">
+        <div className="display">
+          <div className="input">input</div>
+          <div className="result">result</div>
+        </div>
+        <div className="buttons">
+          <div className="row">
+            <button onClick={() => handleButtonClick("7")}>7</button>
+            <button onClick={() => handleButtonClick("8")}>8</button>
+            <button onClick={() => handleButtonClick("9")}>9</button>
+            <button onClick={() => handleButtonClick("/")}>/</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleButtonClick("4")}>4</button>
+            <button onClick={() => handleButtonClick("5")}>5</button>
+            <button onClick={() => handleButtonClick("6")}>6</button>
+            <button onClick={() => handleButtonClick("*")}>*</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleButtonClick("1")}> 1</button>
+            <button onClick={() => handleButtonClick("2")}>2</button>
+            <button onClick={() => handleButtonClick("3")}>3</button>
+            <button onClick={() => handleButtonClick("-")}>-</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleButtonClick("0")}>0</button>
+            <button onClick={() => handleButtonClick(".")}>.</button>
+            <button onClick={() => handleButtonClick("=")}>=</button>
+            <button onClick={() => handleButtonClick("+")}>+</button>
+          </div>
+          <div className="clear-row clear-btn">
+            <button onClick={() => handleButtonClick("C")}>C</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
